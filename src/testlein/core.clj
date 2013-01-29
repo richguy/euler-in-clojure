893c35da93b5766236a9ed749d2d2fc8ba2933e9
Rich Guy <richard.guy@gmail.com>
2013-01-28 22:11:05 -0600

    Test Update

--- src/testlein/core.clj
+++
@@ -1,6 +0,0 @@
-(ns testlein.core)
-
-(defn test
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
--- src/testlein/euler17.clj
+++
@@ -1,8 +0,0 @@
-(ns testlein.euler16
-  (:require clojure.pprint))
-
-;test update
-(defn count-letters [x]
-                (count(clojure.pprint/cl-format true "~r" x)))
-
-
